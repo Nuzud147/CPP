924258a1753490c18910b6c4116cb7f81eb07c9e
Nuzud Binte Olid <nuzud.olid@gmail.com>
2025-05-13 23:49:18 +0600

    Add files via upload

---
+++ print.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main ()
+{
+    std::cout<< "Hello World!"<<'\n';
+    std::cout<< "My name is Nuzud"<<'\n';
+    std::cout<< "This is my first code of cpp"<<'\n';
+    std::cout<< "Let's hope for the best,fingercrossed!"<<'\n';
+
+    return 0;
+}
