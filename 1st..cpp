8ee1e8ca1f1c880d7f638008ed2e48f522ca579a
Nuzud Binte Olid <nuzud.olid@gmail.com>
2025-05-13 23:46:31 +0600

    Add files via upload

---
+++ 1st..cpp
@@ -0,0 +1,15 @@
+#include<iostream> //input output stream
+int main ()
+{
+    //std=standard
+    //::=scope resolution operator
+    //cout=charecter output
+    //new line as endl and '\n'{this is more usuable}
+    //endl=end line
+    std::cout<< "Standard character output using c++" <<std::endl;//new line as endl from <<std::end1
+    std::cout<< "Hey,how are you?" <<'\n';//new line as '\n' from <<'\n'
+    std::cout<< "My name is Nuzud\n";//new line a \n
+    std::cout<< "Today is a rainy day";
+
+    return 0;
+}
