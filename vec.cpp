fad1c12d7249030468df2b145370e17771c65d76
Nuzud Binte Olid <nuzud.olid@gmail.com>
2025-07-13 23:29:34 +0600

    Add files via upload

---
+++ vec.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    vector<int>a(5);
+
+    cout<<"output\n";
+    for(int i=0;i<a.size();i++)
+    {
+        cout<<a[i]<<" ";
+    }
+
+}
