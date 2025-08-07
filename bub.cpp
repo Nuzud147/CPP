18f5cfee4025a48e38e0426956a6f96a09a29bf8
Nuzud Binte Olid <nuzud.olid@gmail.com>
2025-08-07 23:44:34 +0600

    Add files via upload

---
+++ bub.cpp
@@ -0,0 +1,28 @@
+//bubble sort
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        for(int k=0;k<n-1;k++)
+        {
+            if(a[k]>a[k+1])
+            {
+                swap(a[k],a[k+1]);
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
+
