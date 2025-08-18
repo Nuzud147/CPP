e4846d7fa81b77bdb042ce572f340e47a2bddecb
Nuzud Binte Olid <nuzud.olid@gmail.com>
2025-08-18 23:00:30 +0600

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,12 @@
+//double er code
+#include<bits/stdc++.h>
+
+int main ()
+{
+    double point = 1.5555555555;
+
+    std::cout<<std::fixed<<std::setprecision(9);
+    std::cout<<point;
+
+    return 0;
+}
